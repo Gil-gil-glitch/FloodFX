b82b74584a1c3e89d3cbcfe31d4d05496f888535
Gil-gil-glitch <gilsocojp@gmail.com>
2026-07-18 19:59:52 +0900

    Added project structure

---
+++ src/main/java/com/example/floodfx/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.floodfx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/com/example/floodfx/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.floodfx;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
