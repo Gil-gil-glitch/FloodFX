abfc9c6d23410b996910e0bf1413714259b8fd8b
Gil-gil-glitch <gilsocojp@gmail.com>
2026-07-18 20:01:18 +0900

    Defined the CellType for the Arena UI

---
+++ src/main/java/com/example/floodfx/CellType.java
@@ -0,0 +1,20 @@
+package com.example.floodfx;
+
+import javafx.scene.paint.Color;
+
+public enum CellType {
+    OPEN(Color.WHITE),
+    WALL(Color.DARKGRAY),
+    WATER(Color.DODGERBLUE),
+    EXIT(Color.SPRINGGREEN); // Kept your classic Reversi aesthetic!
+
+    private final Color color;
+
+    CellType(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+}
