99ebc8666c63a8e6e77a725e9c5610f83d29021b
Gil-gil-glitch <gilsocojp@gmail.com>
2026-07-18 20:12:57 +0900

    Removed default files

--- src/main/java/com/example/floodfx/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.example.floodfx;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
